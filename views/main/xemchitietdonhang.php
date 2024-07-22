4b1ec502b928c0f371daf1818187263d1e92022b
nhi <nhittnph34670@fpt.edu.vn>
2024-07-22 19:28:55 +0700

    delete list  admin image, password

--- views/main/xemchitietdonhang.php
+++ views/main/xemchitietdonhang.php
@@ -1,17 +1,18 @@
 <div class="page-content bg-white">
-  
-    <div class="dz-bnr-inr style-1 text-center bg-parallax" style="background-image:url('assets/images/banner/bnr4.jpg'); background-size:cover; background-position:center;">
+
+    <div class="dz-bnr-inr style-1 text-center bg-parallax"
+        style="background-image:url('assets/images/banner/bnr4.jpg'); background-size:cover; background-position:center;">
         <div class="container">
             <div class="dz-bnr-inr-entry">
                 <h1>Theo dõi đơn hàng của bạn</h1>
-              
+
                 <nav aria-label="breadcrumb" class="breadcrumb-row">
                     <ul class="breadcrumb">
                         <li class="breadcrumb-item"><a href="index.php">Trang chủ</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Theo dõi đơn hàng của bạn</li>
                     </ul>
                 </nav>
-              
+
             </div>
         </div>
     </div>
@@ -28,7 +29,6 @@
                                 <table class="table table-striped">
                                     <tbody>
                                         <tr>
-
                                             <th>STT</th>
                                             <th>Mã đơn hàng</th>
                                             <th>Ảnh món ăn</th>
@@ -43,63 +43,64 @@
                                         foreach ($chitiet as $key => $chitiet) {
                                             $stt++;
                                         ?>
-                                            <tr>
+                                        <tr>
 
-                                                <td>
-                                                    <?= $stt; ?>
-                                                </td>
+                                            <td>
+                                                <?= $stt; ?>
+                                            </td>
 
-                                                <td>
-                                                    <?= $chitiet['ma_donhang']; ?>
-                                                </td>
-                                                <td>
-                                                    <img width="150" src="uploads/monan/<?= $chitiet['anh_monan']; ?>" alt="">
-                                                </td>
+                                            <td>
+                                                <?= $chitiet['ma_donhang']; ?>
+                                            </td>
+                                            <td>
+                                                <img width="150" src="uploads/monan/<?= $chitiet['anh_monan']; ?>"
+                                                    alt="">
+                                            </td>
 
-                                                <td>
-                                                    <?= $chitiet['soluongmua']; ?>
-                                                </td>
-                                                <td>
-                                                    <?= number_format($chitiet['gia_monan'], 0, ",", '.'); ?> VNĐ
-                                                </td>
-                                                <td>
-                                                    <?= $chitiet['loai_thanhtoan']; ?>
-                                                </td>
-                                                <td>
-                                                    <?= $chitiet['ngaymua']; ?>
-                                                </td>
+                                            <td>
+                                                <?= $chitiet['soluongmua']; ?>
+                                            </td>
+                                            <td>
+                                                <?= number_format($chitiet['gia_monan'], 0, ",", '.'); ?> VNĐ
+                                            </td>
+                                            <td>
+                                                <?= $chitiet['loai_thanhtoan']; ?>
+                                            </td>
+                                            <td>
+                                                <?= $chitiet['ngaymua']; ?>
+                                            </td>
 
-                                                <?php
+                                            <?php
                                                 if ($chitiet["id_trangthai"] == 1) {
                                                 ?>
-                                                    <td class="text-primary">
-                                                        <?= $chitiet["tentrangthai"] ?>
-                                                    </td>
-                                                <?php
+                                            <td class="text-primary">
+                                                <?= $chitiet["tentrangthai"] ?>
+                                            </td>
+                                            <?php
                                                 } else if ($chitiet["id_trangthai"] == 2) {
                                                 ?>
-                                                    <td class="text-primary">
-                                                        <?= $chitiet["tentrangthai"] ?>
-                                                    </td>
-                                                <?php
+                                            <td class="text-primary">
+                                                <?= $chitiet["tentrangthai"] ?>
+                                            </td>
+                                            <?php
                                                 } else if ($chitiet["id_trangthai"] == 3) {
                                                 ?>
-                                                    <td style="color: #66CC33;">
-                                                        <?= $chitiet["tentrangthai"] ?>
-                                                    </td>
-                                                <?php
+                                            <td style="color: #66CC33;">
+                                                <?= $chitiet["tentrangthai"] ?>
+                                            </td>
+                                            <?php
                                                 } else if ($chitiet["id_trangthai"] == 4) {
                                                 ?>
-                                                    <td style="color: #66da98;">
-                                                        <?= $chitiet["tentrangthai"] ?>
-                                                    </td>
-                                                <?php
+                                            <td style="color: #66da98;">
+                                                <?= $chitiet["tentrangthai"] ?>
+                                            </td>
+                                            <?php
                                                 }
                                                 ?>
 
 
 
-                                            </tr>
+                                        </tr>
 
                                         <?php } ?>
 
@@ -108,12 +109,12 @@
 
                             </div>
                         </div>
-                      
+
                     </div>
 
                 </div>
             </div>
         </div>
     </section>
-   
+
 </div>
